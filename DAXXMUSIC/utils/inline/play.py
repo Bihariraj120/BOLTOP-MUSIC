1867b6eb08689ff07be0245592c17f097205315a
GARIB-CODERS <landrepo541@gmail.com>
2024-09-05 23:35:57 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -69,11 +69,11 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(
                 text="✪ʟɪꜰᴇ✪",
-                url=f"https://t.me/+PLTbaq9LInMxZjhl",
+                url=f"https://t.me/CRAZY_OP_01",
             ),
             InlineKeyboardButton(
                 text="✪ᴊᴀɴɴ✪",
-                url=f"https://t.me/+PLTbaq9LInMxZjhl",
+                url=f"https://t.me/PURVI_SUPPORT",
             ),
         ],
         [
